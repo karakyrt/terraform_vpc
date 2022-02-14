d81c964af181e1165d802461bdf1194ebc2bc2c0
karakyrt <akiseleichuk84@gmail.com>
2022-02-14 22:37:56 +0000

    vpc added

---
+++ natgw.tf
@@ -0,0 +1,8 @@
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.main.id
+  subnet_id     = aws_subnet.pub_sub_1.id
+
+  tags = {
+    Name = "${var.env}-${var.app}-nat-gateway"
+  }
+}
